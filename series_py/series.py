50e957053f2dffc36f273fa297265b3f91a3a95b
mohmmadnoorjebreen <mohammadnoormjk1998@gmail.com>
2021-07-26 21:35:48 +0300

    update commit

--- series_py/series.py
+++ series_py/series.py
@@ -1,3 +1,6 @@
+
+# create function that take number and retune the fibonacci series for it
+
 def fibonacci(n):
    if n < 0:
        return 'invalid value'
@@ -7,6 +10,7 @@ def fibonacci(n):
        return(fibonacci(n-1) + fibonacci(n-2))
    
  
+# create function that take number and retune the lucas number for it
    
 def lucas(n):
     if n<0:
@@ -17,15 +21,18 @@ def lucas(n):
         return 1
     return lucas(n-1) + lucas(n-2)
 
+# Combine the two function in general function
 
 def sum_series(n,def1=0,def2=1):
-      if n==0:
+    if n==0:
         return def1
-      if n==1:
+    if n==1:
         return def2
-      return sum_series(n-1,def1,def2) + sum_series(n-2,def1,def2)
+    return sum_series(n-1,def1,def2) + sum_series(n-2,def1,def2)
        
     
- 
+
+
+
 
 
--- tests/test_series_py.py
+++ tests/test_series_py.py
@@ -6,7 +6,7 @@ def test_version():
     assert __version__ == '0.1.0'
   
 
-
+# test fibonacci function by passing a normal number bigerr than zero
 def test_fibonacci():
     expected = 3
     
@@ -14,6 +14,7 @@ def test_fibonacci():
     
     assert expected == actual
   
+# test fibonacci function by passing a negative number 
   
 def test_fibonacci_negative():
     expected = 'invalid value'
@@ -22,6 +23,7 @@ def test_fibonacci_negative():
     
     assert expected == actual
 
+# test fibonacci function by passing a zero
 
 def test_fibonacci_zero():
     expected = 0
@@ -30,14 +32,16 @@ def test_fibonacci_zero():
     
     assert expected == actual
     
+# test lucas function by passing a normal number bigerr than zero
     
 def test_lucas():
-    expected = 2
+    expected = 1
     
-    actual = lucas(0)
+    actual = lucas(1)
     
     assert expected == actual
 
+# test lucas function by passing a negative number 
 
 def test_lucas_negative():
     expected = 'invalid value'
@@ -45,6 +49,8 @@ def test_lucas_negative():
     actual = lucas(-1)
     
     assert expected == actual
+    
+# test lucas function by passing a zero
     
 def test_lucas_zero():
     expected = 2
@@ -54,20 +60,32 @@ def test_lucas_zero():
     assert expected == actual    
     
 
+# test sum_series function by passing a the required parameter only
     
-def sum_series_3():
+def test_sum_series():
     expected = 2
     
     actual = sum_series(3)
     
     assert expected == actual
+    
+# test sum_series function by passing a the required parameter with the two optional parameters as 2 , 1.
+ 
+def test_sum_series_optional():
+    expected = 4
+    
+    actual = sum_series(3,2,1)
+    
+    assert expected == actual
+    
+          
+# test sum_series function by passing a the required parameter with the two optional parameters.
 
-def sum_series_optional():
+def test_sum_series_optional():
     expected = 7
     
     actual = sum_series(3,3,2)
     
     assert expected == actual
-    
     
     
